6f2bb9c8fc4c831058798c240d883166b144b721
Chenning Yu <rainorangelemon@gmail.com>
2025-01-13 14:35:43 -0800

    u

--- bash/experiment.sh
+++ bash/experiment.sh
@@ -7,13 +7,31 @@
 # python -m scripts.run_clevr_rejection num_constraints=4 experiment_name=rejection_clevr_pos_4
 
 # Run experiments with different samplers (ULA and UHMC)
-for sampler in ULA UHMC; do
-    for num_constraints in {1..5}; do
-        python -m scripts.run_clevr_ebm \
-            num_constraints=$num_constraints \
-            experiment_name="ebm_${sampler,,}_clevr_pos_$num_constraints" \
-            ebm.num_samples_per_trial=10 \
-            ebm.sampler_type=$sampler
+# Get number of available GPUs
+NUM_GPUS=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l)
+
+if [ $NUM_GPUS -ge 5 ]; then
+    # Run experiments in parallel across GPUs
+    for sampler in ULA UHMC; do
+        for num_constraints in {1..5}; do
+            LOCAL_RANK=$((num_constraints-1)) python -m scripts.run_clevr_ebm \
+                num_constraints=$num_constraints \
+                experiment_name="ebm_${sampler,,}_clevr_pos_$num_constraints" \
+                ebm.num_samples_per_trial=10 \
+                ebm.sampler_type=$sampler &
+        done
+        wait
     done
-done
+else
+    # Original sequential execution
+    for sampler in ULA UHMC; do
+        for num_constraints in {1..5}; do
+            python -m scripts.run_clevr_ebm \
+                num_constraints=$num_constraints \
+                experiment_name="ebm_${sampler,,}_clevr_pos_$num_constraints" \
+                ebm.num_samples_per_trial=10 \
+                ebm.sampler_type=$sampler
+        done
+    done
+fi
 
