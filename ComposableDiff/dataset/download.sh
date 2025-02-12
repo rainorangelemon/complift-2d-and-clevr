9ef37584e42536c2b437d806a3de23049da287e5
Chenning Yu <rainorangelemon@gmail.com>
2025-02-12 15:53:20 -0800

    simplify

--- ComposableDiff/dataset/download.sh
+++ ComposableDiff/dataset/download.sh
@@ -19,11 +19,6 @@ download_gdrive_file() {
 	echo "Downloaded $filename"
 }
 
-# CLEVR Relation Test Datasets
-download_file "https://www.dropbox.com/s/bfj4wjb4ksic6z2/clevr_generation_1_relations.npz?dl=1" "test_clevr_rel_5000_1.npz"
-download_file "https://www.dropbox.com/s/g59mscx6880j72h/clevr_generation_2_relations.npz?dl=1" "test_clevr_rel_5000_2.npz"
-download_file "https://www.dropbox.com/s/nvc2mdsixi7vu3i/clevr_generation_3_relations.npz?dl=1" "test_clevr_rel_5000_3.npz"
-
 # CLEVR 2D Position Test Datasets
 download_file "https://www.dropbox.com/s/je1nbw463ic1urm/clevr_pos_5000_1.npz?dl=1" "test_clevr_pos_5000_1.npz"
 download_file "https://www.dropbox.com/s/8svd75vw1j7wmzj/clevr_pos_5000_2.npz?dl=1" "test_clevr_pos_5000_2.npz"
